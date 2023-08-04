198423a5a1c5917d99fbe33af6d9f7c9534f826b
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-08-04 11:10:08 -0400

    test: implement test cases for setting and requesting session data via http and via socket instance

---
+++ cypress/e2e/reproduction.cy.js
@@ -0,0 +1,15 @@
+
+describe('Replication test', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Sets session data using http', () => {
+    cy.setSessionViaHttp();
+    cy.getSessionViaHttp();
+  });
+
+  it.skip('Sets session data using socket', () => {
+    cy.setSessionViaSocket(); // type error because req.session is undefined
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,44 @@
+/**
+ * Require & configure socket connection to server
+ */
+const io = require('sails.io.js')(require('socket.io-client'));
+// io.sails.url = 'http://localhost:1337';
+// io.sails.useCORSRouteToGetCookie = false;
+
+Cypress.Commands.add('setSessionViaSocket', () => {
+// Using io to interact with session data fails because req.session is undefined
+  return new Cypress.Promise((resolve, reject) => {
+    io.socket.post('/session', {}, (res, jwres) => {
+      if (jwres.statusCode !== 200) {
+        return reject(new Error('Error setting session'));
+      }
+      return resolve();
+    });
+  });
+});
+
+Cypress.Commands.add('getSessionViaSocket', () => {
+  // Using io to interact with session data fails because req.session is undefined
+  return new Cypress.Promise((resolve, reject) => {
+    io.socket.get('/session', (res, jwres) => {
+      debugger;
+      if (jwres.statusCode !== 200) {
+        return reject(new Error('Error getting session'));
+      }
+      if (!res.hasSession) {
+        return reject(new Error(`Session data was not set; had value: ${res}`));
+      }
+      return resolve();
+    });
+  });
+});
+
+Cypress.Commands.add('setSessionViaHttp', () => {
+  // Session works correctly with http requests (but fails with io)
+  cy.request('POST', 'session');
+});
+
+Cypress.Commands.add('getSessionViaHttp', () => {
+  // Session works correctly with http requests (but fails with io)
+  cy.request('GET', 'session');
+});
